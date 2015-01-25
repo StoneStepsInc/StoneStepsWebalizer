b94d449aaa023b11cd121cb697352ecdb1821c74
Stone Steps <support@stonesteps.ca>
2015-01-25 11:59:54 -0500

    Added comments about character sets and structure of log record components

--- logrec.h
+++ logrec.h
@@ -33,20 +33,40 @@ enum log_type_t {
 };
 
 //
-// log record structure
+// log record
+//
+// 1. URL strings can only contain ASCII characters. Non-ASCII characters must be 
+// converted to UTF-8 before they are percent-encoded. However, some applications
+// in the past used arbitrary character encodings before percent-encoding characters,
+// which may lead to bad characters. For example, the JavaScript escape() function
+// encodes the copyright symbol as %A9 (Latin1) instead of %C2%A9 (UTF-8). If a URL 
+// containing such Latin1 character is found in the log, it cannot be interpreted 
+// as a UTF-8 character sequence.
+//
+// 2. User agent string consists of product name and version characters, which are
+// expected to contai only ASCII characters. Product comments in parentheses, on the 
+// other hand, may contain practically any characters. However, RFC-2616 specifies 
+// that if any of these characters are not Latin1 characers, they are expected to be 
+// encoded as specified in RFC-2047 (i.e. =?charset?encoding?text?=). The latter is 
+// not interpreted in any way in this code.
+//
+// 3. It is not clear what character set is used for user identification.
 //
 struct  log_struct  {
-      string_t   hostname;             // hostname
+      string_t   hostname;             // host name
       string_t   method;               // HTTP method
-      string_t   url;                  // raw request field
-      string_t   refer;                // referrer
-      string_t   agent;                // user agent (browser)
-      string_t   srchargs;             // search string
-      string_t   ident;                // ident string (user)
-      string_t   xsrchstr;             // referrer search string
-      uint64_t     xfer_size;            // xfer size in bytes
-      uint64_t     proc_time;            // URL processing time (ms)
-      tstamp_t   tstamp;               // time stamp
+      string_t   url;                  // requested URL path, without the query
+      string_t   refer;                // referrer URL, without the query
+      string_t   agent;                // user agent
+      string_t   srchargs;             // request query
+      string_t   ident;                // user identification
+      string_t   xsrchstr;             // referrer query
+
+      uint64_t   xfer_size;            // transfer size, in bytes
+      uint64_t   proc_time;            // request processing time (ms)
+
+      tstamp_t   tstamp;               // request time stamp
+
       u_short    port;					   // HTTP port
       u_short    resp_code;            // HTTP response code
 
