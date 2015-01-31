db3c58497c9088c40cbfbae94e658a7a49c1414b
Stone Steps <support@stonesteps.ca>
2015-01-30 23:27:08 -0500

    Minor clean-up of the patch update batch file

--- patches/update.bat
+++ patches/update.bat
@@ -5,6 +5,11 @@ rem grab the script location, so we can run the script from anywhere
 rem
 set dir=%~d0%~p0
 
+rem
+rem Set up robocopy options to mirror patches
+rem
+set options=/DCOPY:T /NS /NC /NFL /NDL /NJS /NJH /NP /PURGE 
+
 rem 
 rem Copy all patch files and the series file
 rem
@@ -12,21 +17,21 @@ rem
 echo.
 
 echo Copying Berkeley DB...
-robocopy /NC /NS /NFL /NDL /NJS /NJH /NP /PURGE %dir%..\..\common\db\.hg\patches\ %dir%db\ *.patch series
+robocopy %options% "%dir%..\..\common\db\.hg\patches" "%dir%db" *.patch series 
 
 echo Copying FreeType...
-robocopy /NC /NS /NFL /NDL /NJS /NJH /NP /PURGE %dir%..\..\common\freetype\.hg\patches\ %dir%freetype\ *.patch series
+robocopy %options% "%dir%..\..\common\freetype\.hg\patches" "%dir%freetype" *.patch series %options%
 
 echo Copying GD...
-robocopy /NC /NS /NFL /NDL /NJS /NJH /NP /PURGE %dir%..\..\common\gd\.hg\patches\ %dir%gd\ *.patch series
+robocopy %options% "%dir%..\..\common\gd\.hg\patches" "%dir%gd" *.patch series %options%
 
 echo Copying PNG...
-robocopy /NC /NS /NFL /NDL /NJS /NJH /NP /PURGE %dir%..\..\common\libpng\.hg\patches\ %dir%libpng\ *.patch series
+robocopy %options% "%dir%..\..\common\libpng\.hg\patches" "%dir%libpng" *.patch series %options%
 
 echo Copying MaxMindDB...
-robocopy /NC /NS /NFL /NDL /NJS /NJH /NP /PURGE %dir%..\..\common\GeoIP\.hg\patches\ %dir%maxminddb\ *.patch series
+robocopy %options% "%dir%..\..\common\GeoIP\.hg\patches" "%dir%maxminddb" *.patch series %options%
 
 echo Copying ZLIB...
-robocopy /NC /NS /NFL /NDL /NJS /NJH /NP /PURGE %dir%..\..\common\zlib\.hg\patches\ %dir%zlib\ *.patch series
+robocopy %options% "%dir%..\..\common\zlib\.hg\patches" "%dir%zlib" *.patch series %options%
 
 echo.
