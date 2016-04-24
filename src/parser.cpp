5c1b96b4dd7d1668db998d60dd63d616d4a86823
Stone Steps <support@stonesteps.ca>
2016-04-24 11:21:50 -0400

    Fixed unescaped percent character in an fprintf call

--- src/parser.cpp
+++ src/parser.cpp
@@ -545,7 +545,7 @@ bool parser_t::parse_apache_log_format(const char *format)
 			case 't':
 				if(paramlen) {
                if(config.verbose)
-					   fprintf(stderr, "Error parsing Apache log format - \"%{format}t\" is not supported\n");
+					   fprintf(stderr, "Error parsing Apache log format - \"%%{format}t\" is not supported\n");
 					goto errexit;
 				}
 				log_rec_fields.push_back(eDateTime);
