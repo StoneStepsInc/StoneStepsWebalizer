4b570b918a9cf38e26b57bcbea5a10f7714d9e9a
Stone Steps <support@stonesteps.ca>
2016-09-10 12:09:59 -0400

    Updated spammer node and hash table comments

--- src/spnode.h
+++ src/spnode.h
@@ -17,13 +17,7 @@
 //
 // Spammer
 //
-// 1. Tracks the IP address of a known spammer. Works as an index into the 
-// hosts table, where all host counters are stored.
-//
 struct spnode_t : public base_node<spnode_t> {
-      public:
-         typedef void (*s_unpack_cb_t)(spnode_t& spnode, void *arg);
-
       public:
          spnode_t(void) : base_node<spnode_t>() {}
          spnode_t(const string_t& host) : base_node<spnode_t>(host) {}
@@ -31,6 +25,13 @@ struct spnode_t : public base_node<spnode_t> {
 
 //
 // Spammers
+//
+// Tracks IP addresses of known spammers. 
+// 
+// This table is not saved in the database and instead every host node read from 
+// the database that has the spammer attribute set is inserted into this table.
+// Keeping spammers in a separate hash table allows us to look-up spammers before 
+// the host node can be processed.
 //
 class sp_hash_table : public hash_table<spnode_t> {
 };
