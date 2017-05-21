a15be676d2826e20e14c0b386ae3ccb5dd3efb02
Stone Steps <support@stonesteps.ca>
2017-05-21 12:58:48 -0400

    Updated version to 4.2.1.15

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #define VERSION_MAJOR           4
 #define VERSION_MINOR           2
 #define EDITION_LEVEL           1
-#define BUILD_NUMBER            12
+#define BUILD_NUMBER            15
 
 //
 // Current numeric application version
