fe72bdcec296d4914a72cb4e9e319efb3680df79
Stone Steps <support@stonesteps.ca>
2017-07-16 10:43:34 -0400

    Removed Highcharts standalone framework file because it is no longer required in Highcharts v4.2

--- src/html_output.cpp
+++ src/html_output.cpp
@@ -126,7 +126,6 @@ void html_output_t::write_js_charts_head(FILE *out_fp, page_type_t page_type)
    if(config.js_charts_paths.empty()) {
       if(config.js_charts == "highcharts") {
          // link to a Highcharts package within the 4.2 release, so we get bug fixes, but no major changes
-         fputs("<script type=\"text/javascript\" src=\"https://code.highcharts.com/4.2/adapters/standalone-framework.js\"></script>\n", out_fp);
          fputs("<script type=\"text/javascript\" src=\"https://code.highcharts.com/stock/4.2/highstock.js\"></script>\n", out_fp);
       }
    }
