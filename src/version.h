8d2dcc8cf2bfb2389f383cdf66de6ecabebc0798
Stone Steps <support@stonesteps.ca>
2017-11-08 08:09:35 -0500

    Updated version to 4.3.0.14

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #define VERSION_MAJOR           4
 #define VERSION_MINOR           3
 #define EDITION_LEVEL           0
-#define BUILD_NUMBER            11
+#define BUILD_NUMBER            14
 
 //
 // Current numeric application version
