1dff9ae344e709c1cb7356c42689101fa4e649bd
Stone Steps <support@stonesteps.ca>
2017-11-10 07:50:12 -0500

    Added a comment for norm_url_str that URL-encoded spaces are decoded

--- src/util_url.cpp
+++ src/util_url.cpp
@@ -77,6 +77,7 @@ char from_hex(char c)
 //
 //  x%25y      -> x%25y        : reserved URL characters are not URL-decoded
 //  x+y        -> x+y          : the plus character is not converted to a space (see above)
+//  x%20y      -> x y          : the space character is URL-decoded 
 //  x%41y      -> xAy          : ASCII alpha-numeric characters are URL-decoded
 //  x%y        -> x%25y        : a misplaced percent character is URL-encoded
 //  xAy        -> xAy          : plain ASCII alpha-numeric characters are not changed
