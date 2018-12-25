206cdab37e7a4cde31c0c597fdb305f60cb6e397
Stone Steps <support@stonesteps.ca>
2018-12-25 11:34:15 -0500

    Updated the script copying GD files to include debug symbols

--- patches/update-gd.bat
+++ patches/update-gd.bat
@@ -33,11 +33,17 @@ if NOT EXIST "%GDDEST%build\native\%%d\" mkdir "%GDDEST%build\native\%%d\"
 xcopy %XCOPYOPTS% "%GDSRC%src\*.h" "%GDDEST%build\native\include\"
 
 xcopy %XCOPYOPTS% "%GDSRC%windows\Debug\gd2.dll" "%GDDEST%build\native\bin\Win32\Debug\"
-xcopy %XCOPYOPTS% "%GDSRC%windows\Release\gd2.dll" "%GDDEST%build\native\bin\Win32\Release\"
-xcopy %XCOPYOPTS% "%GDSRC%windows\Debug64\gd2.dll" "%GDDEST%build\native\bin\x64\Debug\"
-xcopy %XCOPYOPTS% "%GDSRC%windows\Release64\gd2.dll" "%GDDEST%build\native\bin\x64\Release\"
-
 xcopy %XCOPYOPTS% "%GDSRC%windows\Debug\gd2.lib" "%GDDEST%build\native\lib\Win32\Debug\"
+xcopy %XCOPYOPTS% "%GDSRC%windows\Debug\gd2.pdb" "%GDDEST%build\native\bin\Win32\Debug\"
+
+xcopy %XCOPYOPTS% "%GDSRC%windows\Release\gd2.dll" "%GDDEST%build\native\bin\Win32\Release\"
 xcopy %XCOPYOPTS% "%GDSRC%windows\Release\gd2.lib" "%GDDEST%build\native\lib\Win32\Release\"
+xcopy %XCOPYOPTS% "%GDSRC%windows\Release\gd2.pdb" "%GDDEST%build\native\lib\Win32\Release\"
+
+xcopy %XCOPYOPTS% "%GDSRC%windows\Debug64\gd2.dll" "%GDDEST%build\native\bin\x64\Debug\"
 xcopy %XCOPYOPTS% "%GDSRC%windows\Debug64\gd2.lib" "%GDDEST%build\native\lib\x64\Debug\"
+xcopy %XCOPYOPTS% "%GDSRC%windows\Debug64\gd2.pdb" "%GDDEST%build\native\lib\x64\Debug\"
+
+xcopy %XCOPYOPTS% "%GDSRC%windows\Release64\gd2.dll" "%GDDEST%build\native\bin\x64\Release\"
 xcopy %XCOPYOPTS% "%GDSRC%windows\Release64\gd2.lib" "%GDDEST%build\native\lib\x64\Release\"
+xcopy %XCOPYOPTS% "%GDSRC%windows\Release64\gd2.pdb" "%GDDEST%build\native\lib\x64\Release\"
