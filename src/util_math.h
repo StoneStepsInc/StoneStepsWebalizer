f75e62803d129845d78ce9085beb4723ed3079fc
Stone Steps <support@stonesteps.ca>
2019-01-02 22:31:49 -0500

    Added a couple of Doxygen comments for functions computing averages

--- src/util_math.h
+++ src/util_math.h
@@ -18,6 +18,15 @@
 /// @brief  Computes an average from the current average, new `double` value and 
 ///         the new number of items.
 ///
+/// This function is based on the formula that uses the average for the previous
+/// number of items and must be called for every new value to produce correct
+/// results.
+/// ```
+/// ca * (n - 1) + v    ca * n - ca + v         v - ca
+/// ---------------- => --------------- => ca + ------
+///        n                   n                  n
+/// ```
+///
 inline double AVG(double curavg, double value, uint64_t newcnt) {return curavg+(value-curavg)/(double)newcnt;}
 
 ///
@@ -29,6 +38,14 @@ inline double AVG(double curavg, uint64_t value, uint64_t newcnt) {return AVG(cu
 ///
 /// @brief  Computes an average from two average values and corresponding two number
 ///         of items values.
+///
+/// This formula has one less multiplication and one more addition than the original and
+/// used to be faster, but it makes no difference on modern processors.
+/// ```
+/// a1 * n1 + a2 * n2    a1 * n1 + a2 * n2 + a1 * n2 - a1 * n2         (a2 - a1) * n2
+/// ----------------- => ------------------------------------- => a1 + --------------
+///      n1 + n2                        n1 + n2                           n1 + n2
+/// ```
 ///
 inline double AVG2(double a1, uint64_t n1, double a2, uint64_t n2) {return a1+(a2-a1)*((double)n2/((double)n1+(double)n2));}
 
