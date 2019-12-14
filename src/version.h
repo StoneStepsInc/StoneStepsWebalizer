c161f3a65ae332cf17f7a4b16a2e8baeaef4efe4
Stone Steps <support@stonesteps.ca>
2019-12-14 12:54:00 -0500

    Updated version to 6.0.0.5

--- src/version.h
+++ src/version.h
@@ -17,7 +17,7 @@
 #define VERSION_MAJOR           6
 #define VERSION_MINOR           0
 #define EDITION_LEVEL           0
-#define BUILD_NUMBER            3
+#define BUILD_NUMBER            5
 
 //
 // Current numeric application version
