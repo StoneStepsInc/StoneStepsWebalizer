921870bb37e95e7308336d6f626016b4c8c911b8
Stone Steps <support@stonesteps.ca>
2022-07-03 14:28:42 -0400

    Updated version to 6.3.0

--- src/version.h
+++ src/version.h
@@ -15,7 +15,7 @@
 // Current application version components
 //
 #define VERSION_MAJOR           6
-#define VERSION_MINOR           2
+#define VERSION_MINOR           3
 #define VERSION_PATCH           0
 
 //
